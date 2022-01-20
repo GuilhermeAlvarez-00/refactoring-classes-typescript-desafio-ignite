c0397046962e210249341d9fbc7bc696d4526ee6
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-20 16:18:26 -0300

    refatorando componente modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,50 @@
-import { Component } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
+interface ModalProps {
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+export default function Modal({ children, isOpen, setIsOpen }: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(isOpen)
+
+  useEffect(() => {
+    setModalStatus(isOpen)
+  }, [isOpen])
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
+
+/* class Modal extends Component {
   constructor(props) {
     super(props);
 
@@ -55,4 +98,4 @@ class Modal extends Component {
   }
 };
 
-export default Modal;
+export default Modal; */
